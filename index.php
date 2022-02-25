6f3e372325c9f28533ad8b5a1c74c597ea312c52
Jorge Moron <moronjorge94@gmail.com>
2022-02-25 09:19:55 -0400

    indicaciones

--- index.php
+++ index.php
@@ -11,10 +11,10 @@ $token  = "3d2b8cbd544a53850164f8f79fd8d3cc";
 $twilio = new Client($sid, $token); 
 
 $message = $twilio->messages 
-                  ->create("whatsapp:+59169092272", // para 
+                  ->create("whatsapp:+59169092272", // telefono personal configurado en twilio que recibira los mensajes(cambiar telefono)
                   array( 
-                    "from" => "whatsapp:+14155238886",       
-                    "body" => "Hola Jorge" 
+                    "from" => "whatsapp:+14155238886",  // telefono de prueba que te da twilio(no cambiar)     
+                    "body" => "Probando mensaje" // mensaje que se enviara por whatsapp 
                 ) 
        ); 
 
